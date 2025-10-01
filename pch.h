caf14231a2a7f97ef5b77eb9e06890dd507cedf1
Chava <chavadvorah@github.com>
2025-10-01 16:08:38 -0500

    Fixed issues with clangd by adding a few includes. Added a cleaning script.

--- pch.h
+++ pch.h
@@ -1,8 +1,19 @@
-﻿#pragma once
+#pragma once
+
+#include <coroutine>
+#include <utility>
 #include <unknwn.h>
+#include <windows.h>
+
+#ifdef GetCurrentTime
+#  undef GetCurrentTime
+#endif
+
 #include <winrt/Windows.Foundation.h>
 #include <winrt/Windows.Foundation.Collections.h>
+#include <winrt/Microsoft.UI.Windowing.h>
 #include <winrt/Microsoft.UI.Xaml.h>
+#include <winrt/Microsoft.UI.Xaml.Media.Animation.h>
 
 namespace wf = winrt::Windows::Foundation;
 namespace mux = winrt::Microsoft::UI::Xaml;
---
+++ scripts/clean.ps1
@@ -0,0 +1,1 @@
+Remove-Item -Recurse -Force -ErrorAction SilentlyContinue .vs, _build, "Generated Files", x64, XamlExtensions
